da93106115ba2adb1d689b47b609946ef8e58a99
Darlan Silva <darlansilv4@gmail.com>
2023-02-15 23:18:59 -0300

    Numbers

---
+++ numbers/index.js
@@ -0,0 +1,10 @@
+let num1 = 10.4234235235235;
+
+// arrendondar
+console.log(num1.toFixed(2))
+
+// verificando se é um inteiro
+console.log(Number.isInteger(num1));
+
+// verificando se não é um número, se não for retornará true
+console.log(Number.isNaN(num1));
