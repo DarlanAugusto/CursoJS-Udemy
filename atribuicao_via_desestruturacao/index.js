93f343bd15db6cc9b84387c13721090d46508a9f
Darlan Silva <darlansilv4@gmail.com>
2023-02-16 18:59:24 -0300

    Atribuição Via Desestruturação (Arrays)

---
+++ atribuicao_via_desestruturacao/index.js
@@ -0,0 +1,11 @@
+const numbers = [
+    [1, 2, 3], // list1
+    [4, 5, 6], // list2
+    [7, 8, 9]  // list3
+]
+
+const [list1, list2, list3] = numbers;
+
+const [number1, number2, number3] = list2;
+
+console.log(number2);
