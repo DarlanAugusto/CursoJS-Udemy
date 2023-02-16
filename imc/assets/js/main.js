e3aa02c6776ac0aa8e4ecb9158db87b8faec90d4
Darlan Silva <darlansilv4@gmail.com>
2023-02-16 18:11:04 -0300

    Exercício IMC, para fixar todo o conteúdo visto até agora

---
+++ imc/assets/js/main.js
@@ -0,0 +1,53 @@
+const resultCalc = document.querySelector(".resultCalc");
+const form = document.querySelector(".form");
+const calcBtn = form.querySelector("button");
+
+calcBtn.addEventListener("click", calculateImc);
+
+function calculateImc(event) {
+    event.preventDefault();
+
+    const weight = form.querySelector("#weight");
+    const height = form.querySelector("#height");
+    let resultImc;
+    let levelImc
+
+    resultCalc.innerHTML = '';
+    resultCalc.style.background = 'transparent';
+
+    if(!weight.value || isNaN(weight.value)) {
+        resultCalc.innerHTML = "Peso inválido!";
+        resultCalc.style.background = "var(--error-color)";
+        return;
+    }
+
+    if(!height.value || isNaN(height.value)) {
+        resultCalc.innerHTML = "Altura inválida!";
+        resultCalc.style.background = "var(--error-color)";
+        return;
+    }
+
+    resultImc = weight.value / (height.value ** 2);
+
+    if(resultImc < 18.5) {
+        levelImc = 'Abaixo do Peso';
+    } 
+    else if (resultImc >= 18.5 && resultImc < 25) {
+        levelImc = 'Peso normal';
+    }
+    else if (resultImc >= 25 && resultImc < 30) {
+        levelImc = 'Sobrepeso';
+    }
+    else if (resultImc >= 30 && resultImc < 35) {
+        levelImc = 'Obesidade grau I';
+    }
+    else if (resultImc >= 35 && resultImc < 40) {
+        levelImc = 'Obesidade grau II';
+    }
+    else if (resultImc > 40) {
+        levelImc = 'Obesidade grau III';
+    }
+
+    resultCalc.innerHTML = `Seu IMC é: ${resultImc.toFixed(2)} (${levelImc})`;
+    resultCalc.style.background = "var(--success-color)";
+}
