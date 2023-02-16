6836e11601c934525c6ca3e975873ffa9c8e851b
Darlan Silva <darlansilv4@gmail.com>
2023-02-15 23:01:04 -0300

    Exercício Manipulação de String, e alterando DOM com JS

---
+++ exercicio_manipulacao_string/js/script.js
@@ -0,0 +1,10 @@
+let nome = prompt('Digite seu nome completo:');
+document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra 'G' no seu nome? ${nome.indexOf('g')}<br />`;
+document.body.innerHTML += `Qual o último índice da letra 'L' no seu nome? ${nome.lastIndexOf('l')}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
