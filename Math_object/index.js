88e6a41b963870419e8f3a4d018c1bd5c0504732
Darlan Silva <darlansilv4@gmail.com>
2023-02-15 23:40:23 -0300

    Objeto Math do JS

---
+++ Math_object/index.js
@@ -0,0 +1,17 @@
+let num1 = 7.4548957;
+
+// arredonda um número para baixo, obtendo um valor inteiro
+console.log(Math.floor(num1));
+
+// arredonda um número para cima, obtendo um valor inteiro
+console.log(Math.ceil(num1));
+
+// arrendonda um número para o mais proximo, obtendo um valor inteiro
+console.log(Math.round(num1))
+
+// recebe um array de numbers e retorna/pega o maior numero da sequencia
+console.log(Math.max(1, 2, 3, 50, 60, 34, 500, 45.039));
+
+// pega um numero aleatório
+console.log(Math.round(Math.random() * 10));
+
