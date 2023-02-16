455dbaa4aa1718c30cc15f9e873b53d38bdb8ce4
Darlan Silva <darlansilv4@gmail.com>
2023-02-15 21:53:31 -0300

    Exercício com Prompt JS

---
+++ exercicio_prompt/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Digite um Número:");
+let num2 = prompt("Digite outro Número:");
+num1 = Number(num1);
+num2 = parseInt(num2);
+let resutado = num1 + num2;
+alert(`${num1} + ${num2}\nO resultado é: ${resutado}`);
