16a797468f53103dbb5132d55982b02c24ab6b9e
Darlan Silva <darlansilv4@gmail.com>
2023-02-18 22:15:17 -0300

    Promises (async e await)

--- promisses/index.js
+++ promisses/index.js
@@ -1,4 +1,4 @@
-function rand(min, max) {
+function rand(min = 0, max = 3) {
   min *= 1000;
   max *= 1000;
 
@@ -7,23 +7,49 @@ function rand(min, max) {
 
 function aguarde(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if(typeof msg !== "string") reject(new TypeError("Não é do tipo string"));
     setTimeout(() => {
+      if(typeof msg !== "string") {
+        reject(new TypeError("Não é do tipo string"));
+        return;
+      } 
+      
       resolve(msg);
+      return;
     }, tempo)
   })
 }
 
 
 
-aguarde("Finalizou.", rand(1 ,3))
-  .then((response) => {
-    console.log(response);
-    return aguarde(123565555, rand(1, 3))
-  })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
+// aguarde("Finalizou.", rand(1 ,3))
+//   .then((response) => {
+//     console.log(response);
+//     return aguarde(123565555, rand(1, 3))
+//   })
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+
+
+async function executaPromises() {
+  try {
+    for(let i = 0; i <= 100; i++) {
+      if(i < 10) i = `0${i}`;
+      
+      const fase = await aguarde(`${i}%`, rand(0, .050));
+      console.log(fase);
+    }
+    console.log("Concluido!");
+  } 
+  catch(error) {
     console.log(error);
-  })
+    return;
+    
+  }
+
+}
+
+executaPromises();
