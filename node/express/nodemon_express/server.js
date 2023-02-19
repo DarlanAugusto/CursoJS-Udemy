7960391fc46939bff103e89890a89922f6c77c25
Darlan Silva <darlansilv4@gmail.com>
2023-02-19 19:00:08 -0300

    Express + Nodemon

---
+++ node/express/nodemon_express/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+      Usuário: <input type="text" name="nome">
+      <button type="submit">Enviar</button>
+    </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send("Recebi o Form!")
+});
+
+app.get('/home', (req, res) => {
+  res.send("Estou na Home!");
+});
+
+app.listen(3333, () => {
+  console.log('Servidor iniciado na porta: 3333.\nAcesse: http://localhost:3333');
+});
