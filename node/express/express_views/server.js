79293f9f88bcf8296a30357a33c3a5da3fa10566
Darlan Silva <darlansilv4@gmail.com>
2023-02-19 21:16:38 -0300

    Express Views

---
+++ node/express/express_views/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes  = require('./routes');
+const path    = require('path');
+const app     = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Servidor iniciado na porta: 3333.\nAcesse: http://localhost:3333');
+});
